9c50e2e42d6ef9e42beb6a6a891af4dfecb3ce92
AustinTheWitch <rdorriety@gmail.com>
2025-10-28 06:04:01 -0400

    First Steps

---
+++ player/player.gd
@@ -0,0 +1,4 @@
+extends Node
+class_name player_base
+
+static var inventory: Dictionary
---
+++ reagents/reagaent.gd
@@ -0,0 +1,9 @@
+extends Node
+class_name reagent_base
+
+var tags: Array[String]
+var sprite: Texture2D
+
+#var dicovered: bool
+#var description: String
+#var keywords: Array[String]
